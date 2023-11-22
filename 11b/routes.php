55d016aae4181d055a154526d62c86b41f9dddf9
Priya Menezes <113142516+pmenezes3094@users.noreply.github.com>
2023-11-22 11:06:50 -0800

    add wrking code

---
+++ 11b/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+$router->get('/11b/public/', 'controllers/index.php');
+$router->get('11b/about', 'controllers/about.php');
+$router->get('11b/contact', 'controllers/contact.php');
+
+$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/note', 'controllers/notes/show.php');
+$router->delete('/note', 'controllers/notes/destroy.php');
+
+$router->get('/note/edit', 'controllers/notes/edit.php');
+$router->patch('/note', 'controllers/notes/update.php');
+
+$router->get('/notes/create', 'controllers/notes/create.php');
+$router->post('/notes', 'controllers/notes/store.php');
+
